6cbcdc6e3174145d83a64ed3fad6fd2c34941f27
Sebastian <sebb@sebb767.de>
2017-10-16 23:59:40 +0200

    Switched alt and super (they were changed for some reason)

--- keyboards/b_face/b_face.h
+++ keyboards/b_face/b_face.h
@@ -28,7 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
   K03, K13, K23, K33, K43, K53, K26, KC6, KC7, K27, KA3, KB3, KC3, KD3, \
   K02, K12, K22, K32, K42, K52, K36, KD6, KD7, K37, KA2, KB2,      KD2, \
   K01, K11, K21, K31, K41, K51, K46, KE6, KE7, K47, KA1, KB1, \
-  K00, K20, K10,           K56,                KA0, K57, KB0, KC0  \
+  K00, K10, K20,           K56,                K57, KA0, KB0, KC0  \
 ){ \
   { K00,   K10,   K20,   KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KA0,   KB0,   KC0,   KC_NO, KC_NO }, \
   { K01,   K11,   K21,   K31,   K41,   K51,   KC_NO, KC_NO, KC_NO, KC_NO, KA1,   KB1,   KC_NO, KC_NO, KC_NO }, \
