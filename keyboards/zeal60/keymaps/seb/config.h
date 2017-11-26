acbee13df4006e25708ee93ace02e88800db4a1b
Sebastian <sebb@sebb767.de>
2017-11-26 19:48:16 +0100

    Changed enum to defines and added CAPS+Space Cadet=Normal Shift

--- keyboards/zeal60/keymaps/seb/config.h
+++ keyboards/zeal60/keymaps/seb/config.h
@@ -20,10 +20,9 @@
 #define USE_ISO_ENTER 0
 
 // extra keycodes
-enum layout_seb_keycodes {
-	FN_LCTL = 0x5FE0, // left ctl; disable layer 3
-	FN_RCTL, 		  // right ctl; disable layer 3
-	KC_CTW			  // close tab (crtl+w)
-};
+
+#define FN_LCTL 0x5FE0 // left ctl; disable layer 3
+#define FN_RCTL 0x5FE1 // right ctl; disable layer 3
+#define KC_CTW  0x5FE1 // close tab (crtl+w)
 
 #endif //CONFIG_USER_H 
